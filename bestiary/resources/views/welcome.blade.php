7b1ad832a4562986746e3c03ce379037548f536b
lf <1006554@hr.nl>
2021-09-15 20:30:56 +0200

    Initial commit

---
+++ bestiary/resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+
+    </head>
+    <body>
+    <p>Best of the best Bestiary upcoming</p>
+    </body>
+</html>
