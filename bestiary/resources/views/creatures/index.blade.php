4c30827078010dafc360c5a63d46a4db4e2066c2
lf <1006554@hr.nl>
2021-09-21 22:19:39 +0200

    21-9

--- bestiary/resources/views/creatures/index.blade.php
+++ bestiary/resources/views/creatures/index.blade.php
@@ -3,13 +3,11 @@
 @section('content')
     <div class="row">
         <div class="col-md-12">
-            <H1 class="Title">Hello world</H1>
+            <H1 class="Title">Bestiary</H1>
         </div>
     </div>
     <div class="row">
         <div class="col-md-12">
-            <h1></h1>
-            <li>creatures{{$creatures->name}}</li>
         </div>
     </div>
     <div
--- bestiary/resources/views/partials/header.blade.php
+++ bestiary/resources/views/partials/header.blade.php
@@ -1,28 +1,19 @@
-<div class="col-md-8">
-    <h1> Better title Pending </h1>
+<div class="navigation">
+<div class="dropdown">
+    <button class="dropbtn">Categories</button>
+    <div class="dropdown-content">
+        <a href="#">Mythology</a>
+        <a href="#">Folklore</a>
+        <a href="#">Cryptids</a>
+        <a href="#">Aliens</a>
+    </div>
 </div>
-<ul class="nav justify-content-center">
-    <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Categories</a>
-        <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Mythological creatures</a>
-            <a class="dropdown-item" href="#">Folktale creatures</a>
-            <a class="dropdown-item" href="#">Cryptids</a>
-            <a class="dropdown-item" href="#">Aliens</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
-        </div>
-    </li>
-    <li>
-        <form class="form-inline">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link login" href="#">Log in</a>
-    </li>
-    <li class="nav-item">
-        <a class="nav-link register" href="#">Register</a>
-    </li>
-</ul>
+
+<div class="search-bar">
+    <form class="form-inline">
+        <input class="form-control" type="search" placeholder="Search" label="Search"></form>
+        <button class="searchbtn" type="submit">Search</button>
+    </form>
+</div>
+</div>
+
