6561033adc932cb00928d4ea366362738c47f23d
lf <1006554@hr.nl>
2021-10-07 15:55:14 +0200

    Made the creature edit and create

---
+++ bestiary/app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Creature;
+use Illuminate\Http\Request;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function profile(){
+        return view(users.index);
+    }
+
+    public function favorites(){
+        return view();
+    }
+
+    public function toggle(){
+        return view();
+    }
+
+}
