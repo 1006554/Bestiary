edca422a4ddad463a61184b51e54666b8e25dd8f
lf <1006554@hr.nl>
2021-10-07 15:55:14 +0200

    Made the creature edit and create

--- bestiary/app/Http/Controllers/UserController.php
+++ bestiary/app/Http/Controllers/UserController.php
@@ -2,63 +2,22 @@
 
 namespace App\Http\Controllers;
 use App\Models\Creature;
-use App\Models\User;
-use App\Models\Comment;
+use Illuminate\Http\Request;
+
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * shows the username on the users page
-     */
-    public function show($id) {
-        $user = User::find($id)->get();
-            if($id == auth()->user()->id){
-            return view('users.index', compact('user'));
-        }else {
-            return redirect('home')->with('status','Not authorized to acces this page.');
-        }
+    public function profile(){
+        return view(users.index);
     }
 
-    /**
-     * edits the user's data.
-     */
-    public function edit($id){
-        if($user = User::find($id)){
-            if($id == auth()->user()->id){
-                return view('users.edit', compact('user'));
-            } else {
-                return redirect ('home')->with('not authorized to access this page.');
-            }
-        }
+    public function favorites(){
+        return view();
     }
 
-    /**
-     * updates the articles based on their category.
-     */
-
-    public function update(Request $request, $id){
-        $request->validate([
-            'name' => 'required',
-            'email' => 'required',
-        ]);
-        $user = User::find($id)->update([
-            'name' => $request->name,
-            'email' => $request->email,
-        ]);
-
-        return view('users.index', compact('user'));
+    public function toggle(){
+        return view();
     }
-
-    /**
-     * sends the user data with the comment.
-     */
-    public function comments($id){
-        if($users = Comment::where('user_id', $id)->get()){
-            return view('partials.comment', compact('users'));
-        }
-    }
-
-
 
 }
