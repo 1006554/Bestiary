c2b2e87d3f8040ab3015fb625ccf2ab1e2f01865
lf <1006554@hr.nl>
2021-10-17 14:30:57 +0200

    Gonna start doing the favorite system at some point.

--- bestiary/resources/views/blog/create.blade.php
+++ bestiary/resources/views/blog/create.blade.php
@@ -11,32 +11,29 @@
 
                         <h1>Add a new creature</h1>
                         <div class="form-group">
-
                             <label for="name">Name:</label>
-
                             <input type="text" id="name" name="name" class="form-control">
-
+                            @error('name')
+                            <div class="error">Please enter a name.</div>
+                            @enderror
                         </div>
-
-
 
                         <div class="form-group">
-
                             <label for="description">Description</label>
-
                             <input type="text" id="description" name="description" class="form-control">
-
+                            @error('description')
+                            <div class="error">Please fill in a description.</div>
+                            @enderror
                         </div>
-
-
 
                         <div class="form-group">
                             <label>Upload Your Image
                                 <input type="file" name="image" />
+                                @error('image')
+                                <div class="error">Please add an image.</div>
+                                @enderror
                             </label>
                         </div>
-
-
 
                         <div class="form-group">
                             <input type="radio" id="mythology" name="tag" value="Mythology">
@@ -45,8 +42,12 @@
                             <label for="cryptid">Cryptid</label>
                             <input type="radio" id="aliens" name="tag" value="Aliens">
                             <label for="aliens">Aliens</label><br>
+                            @error('tag')
+                            <div class="error">Please select a category.</div>
+                            @enderror
                         </div>
-                        <button type="submit" class="btn btn-primary">Add</button>
+
+                        <button type="submit" class="btn btn-primary" value="Submit">Add</button>
                     </form>
                         @else
                         You are not signed in.
